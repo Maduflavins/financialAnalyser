57121312e9521c2b609e6b75905df2080def1d24
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-08-26 13:54:39 +0100

    inital commit: implemented SRP

---
+++ src/com/company/flavins/Main.java
@@ -0,0 +1,14 @@
+package com.company.flavins;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    final BankTransactionAnalyzerSimple bankTransactionAnalyzerSimple = new BankTransactionAnalyzerSimple();
+
+	    final BankStatementParser bankStatementParser = new BankStatementCSVParser();
+
+	    bankTransactionAnalyzerSimple.analyze(args[0], bankStatementParser);
+    }
+}
